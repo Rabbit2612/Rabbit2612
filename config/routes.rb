df58ea47b7723360d36e5aacddfa72ddc04efb38
thao <thaovu06022003@gmail.com>
2024-04-12 10:28:33 +0700

    Polishing plate boiler content

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  resources :books
+  resources :subjects
   authenticate :user, ->(u) { u.admin? } do
     mount Sidekiq::Web => '/sidekiq'
   end
@@ -10,9 +13,6 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "home#index"
-
-  resources :books
-  resources :subjects
+  root "books#index"
 
 end
